76377b91e6d4c55fd0d5c466dd2d17c824c6737b
Fereizqo Sulaiman <fereizqoahmad@sci.ui.ac.id>
2022-02-17 19:58:06 +0700

    Initial Commit

---
+++ DummyProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DummyProject
+//
+//  Created by Fereizqo Sulaiman on 17/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DummyProject/DummyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyProjectApp.swift
+//  DummyProject
+//
+//  Created by Fereizqo Sulaiman on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct DummyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
